6d2fea40c302c94bbed6bf7b72d9d15e3566d1c4
Tom Wagg <tomjwagg@gmail.com>
2025-12-04 15:35:23 -0500

    underworld binaries

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import astropy.units as u
 
 
 def get_kinematics(pops):
@@ -37,4 +38,57 @@ def get_kinematics(pops):
             "BH": bh_vel,
             "CO": co_vel,
         }
+
+        kinematics[pop.label]["escaped"] = {}
+        for co_type, pos, vel in zip(
+            ["NS", "BH", "CO"],
+            [ns_pos, bh_pos, co_pos],
+            [ns_vel, bh_vel, co_vel],
+        ):
+            V = np.linalg.norm(vel.to(u.km/u.s).value, axis=1)
+            escaped = V >= np.sqrt(-2 * pop.galactic_potential(pos.T)).to(u.km / u.s).value
+
+            kinematics[pop.label]["escaped"][co_type] = escaped
+
     return kinematics
+
+
+def get_underworld_binaries(pops, verbose=False):
+    co_binary_labels = ["BH-BH", "BH-NS", "BH-WD", "BH-Star", "NS-NS", "NS-WD", "NS-Star"]
+    co_binary_kstar_groups = [
+        ([14], [14]),
+        ([14], [13]),
+        ([14], [10, 11, 12]),
+        ([14], list(range(0, 10))),
+        ([13], [13]),
+        ([13], [10, 11, 12]),
+        ([13], list(range(0, 10))),
+    ]
+
+    underworld_binaries = {}
+    for pop in pops:
+        underworld_binaries[pop.label] = {}
+        co_binaries = pop.final_bpp[
+            ((pop.final_bpp["kstar_1"].isin([13, 14])) | (pop.final_bpp["kstar_2"].isin([13, 14]))) &
+            (pop.final_bpp["sep"] > 0)
+        ]
+
+        underworld_binaries[pop.label] = {
+            label: co_binaries[
+                ((co_binaries["kstar_1"].isin(kstar_group[0]))
+                 & (co_binaries["kstar_2"].isin(kstar_group[1]))) |
+                ((co_binaries["kstar_1"].isin(kstar_group[1]))
+                 & (co_binaries["kstar_2"].isin(kstar_group[0])))
+            ] for (label, kstar_group) in zip(co_binary_labels, co_binary_kstar_groups)
+        }
+
+        scale_up = 6e10 / pop.mass_binaries
+
+        if verbose:
+            print(f"{pop.label} Underworld Binaries (scale up by {scale_up:.0f}x):")
+            for label, table in underworld_binaries[pop.label].items():
+                print(f"  {label}:{' ' * (9 - len(label))} {len(table):.0f}  \t{len(table) * scale_up:.1e} (scaled)")
+                if label == "BH-Star":
+                    print()
+            print()
+    return underworld_binaries
