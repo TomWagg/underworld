40639cc569b6cf5177fc331811d8dd73074949ea
Tom Wagg <tomjwagg@gmail.com>
2026-02-25 21:18:16 -0500

    update postprocess with new sep/no kick stuff

--- src/postprocess.py
+++ src/postprocess.py
@@ -10,7 +10,7 @@ files = ['fiducial',
          'fryer_rapid', 'mandel_muller', 'maltsev_fallback_0.0', 'maltsev_fallback_0.25',
          'maltsev_fallback_0.5', 'maltsev_fallback_0.75', 'maltsev_fallback_1.0',
          'maltsev_pf_prob_0.0', 'maltsev_pf_prob_1.0',
-         'bhflag_3', 'kickflag_1']
+         'bhflag_0', 'bhflag_3', 'kickflag_1', 'time-evolving-pot']
 
 BASE_PATH = "/mnt/ceph/users/twagg/underworld/"
 postprocess_folder = os.path.join(BASE_PATH, "postprocessed")
@@ -49,6 +49,35 @@ for file in files:
         }
         masses[pop.label]["CO"] = np.concatenate((masses[pop.label]["NS"], masses[pop.label]["BH"]))
 
+    print("Recording binarity")
+
+    sep = {}
+    primary = {}
+    for pop in pops:
+        sep[pop.label] = {
+            "BH": np.concatenate((
+                pop.final_bpp["sep"][pop.final_bpp["kstar_1"] == 14],
+                pop.final_bpp["sep"][pop.final_bpp["kstar_2"] == 14],
+            )),
+            "NS": np.concatenate((
+                pop.final_bpp["sep"][pop.final_bpp["kstar_1"] == 13],
+                pop.final_bpp["sep"][pop.final_bpp["kstar_2"] == 13],
+            )),
+        }
+        sep[pop.label]["CO"] = np.concatenate((sep[pop.label]["NS"], sep[pop.label]["BH"]))
+
+        primary[pop.label] = {
+            "BH": np.concatenate((
+                np.repeat(True, len(pop.final_bpp["kstar_1"][pop.final_bpp["kstar_1"] == 14])),
+                np.repeat(False, len(pop.final_bpp["kstar_2"][pop.final_bpp["kstar_2"] == 14])),
+            )),
+            "NS": np.concatenate((
+                np.repeat(True, len(pop.final_bpp["kstar_1"][pop.final_bpp["kstar_1"] == 13])),
+                np.repeat(False, len(pop.final_bpp["kstar_2"][pop.final_bpp["kstar_2"] == 13])),
+            )),
+        }
+        primary[pop.label]["CO"] = np.concatenate((primary[pop.label]["NS"], primary[pop.label]["BH"]))
+
     print("Saving processed data...")
 
-    helpers.save_postprocessed_data(pops, [postprocess_file], kinematics, masses, bin_nums)
+    helpers.save_postprocessed_data(pops, [postprocess_file], kinematics, masses, bin_nums, sep, primary)
