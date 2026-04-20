6dd0f1e9b0bbd46848be100cfae12ce8813db69e
Tom Wagg <tomjwagg@gmail.com>
2026-04-20 16:24:09 -0400

    update submitters to use simpler naming convention

--- slurm/_submit_initC_variations.sh
+++ slurm/_submit_initC_variations.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-N_PARTS=34
+N_PARTS=5
 
 # submit job arrays for each of the variation_*.slurm files in this directory
 # wait 1 second between submissions to avoid overwhelming the scheduler
 
-for slurm_file in variation_mt_*.slurm; do
-    echo Submitting job array for $slurm_file with $MAX_PARTS parts...
+for slurm_file in variation_initC_*.slurm; do
+    echo Submitting job array for $slurm_file with $N_PARTS parts...
     
-    # submit the job array with the appropriate number of parts
+    # submit the job array with the appropriate number of parts (0 to N_PARTS - 1)
     sbatch --array=0-$((N_PARTS - 1)) "$slurm_file"
 
     # wait 1 second before submitting the next job array
--- slurm/_submit_main_variations.sh
+++ slurm/_submit_main_variations.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-MAX_PARTS=33
+N_PARTS=34
 
-# submit job arrays for each of the variation_*.slurm files in this directory
-# wait 1 second between submissions to avoid overwhelming the scheduler
-
-for slurm_file in variation_*.slurm; do
-    # skip any file that includes "alpha", "beta", or "qcrit" in the name
-    if [[ "$slurm_file" == *"alpha"* || "$slurm_file" == *"beta"* || "$slurm_file" == *"qcrit"* ]]; then
-        continue
-    fi
+for slurm_file in variation_main_*.slurm; do
     echo Submitting job array for $slurm_file with $MAX_PARTS parts...
     
     # # submit the job array with the appropriate number of parts
-    sbatch --array=0-$MAX_PARTS "$slurm_file"
+    sbatch --array=0-$((N_PARTS - 1)) "$slurm_file"
 
     # wait 1 second before submitting the next job array
     sleep 0.5
