ab816c9210b408b3fb94ead054ed73b951bd7eea
Tom Wagg <tomjwagg@gmail.com>
2026-06-17 14:35:47 -0400

    smarter submitters with first parts

--- slurm/_submit_initC_variations.sh
+++ slurm/_submit_initC_variations.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-N_PARTS=5
+FIRST_PART=5
+N_PARTS=34
 
 # submit job arrays for each of the variation_*.slurm files in this directory
 # wait 1 second between submissions to avoid overwhelming the scheduler
@@ -9,7 +10,7 @@ for slurm_file in variation_initC_*.slurm; do
     echo Submitting job array for $slurm_file with $N_PARTS parts...
     
     # submit the job array with the appropriate number of parts (0 to N_PARTS - 1)
-    sbatch --array=0-$((N_PARTS - 1)) "$slurm_file"
+    sbatch --array=$FIRST_PART-$((N_PARTS - 1)) "$slurm_file"
 
     # wait 1 second before submitting the next job array
     sleep 0.5
--- slurm/_submit_main_variations.sh
+++ slurm/_submit_main_variations.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-N_PARTS=5
+FIRST_PART=5
+N_PARTS=34
 
 for slurm_file in variation_main_*.slurm; do
     echo Submitting job array for $slurm_file with $MAX_PARTS parts...
     
     # # submit the job array with the appropriate number of parts
-    sbatch --array=0-$((N_PARTS - 1)) "$slurm_file"
+    sbatch --array=$FIRST_PART-$((N_PARTS - 1)) "$slurm_file"
 
     # wait 1 second before submitting the next job array
     sleep 0.5
